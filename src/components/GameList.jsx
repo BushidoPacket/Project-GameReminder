787cc6b7d4b9a997b86632be4cb5e434f43fc326
polluxik <malca9616@gmail.com>
2023-07-10 19:07:19 +0200

    main with updated GameList.jsx file

---
+++ src/components/GameList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import classes from "./GameList.module.css";
+import games from "../database.json";
+
+export const GameList = ({ search }) => {
+  const [gameData, setGameData] = useState([]);
+
+  useEffect(() => {
+    setGameData(games.game);
+  }, []);
+
+  const filteredGames = gameData.filter((item) => {
+    return (
+      item.name.toLowerCase().includes(search.toLowerCase()) ||
+      item.platform.toLowerCase().includes(search.toLowerCase()) ||
+      item.release.toLowerCase().includes(search.toLowerCase()) ||
+      item.company.toLowerCase().includes(search.toLowerCase())
+    );
+  });
+
+  return (
+    <>
+      <div className={classes.gameList}>
+        {filteredGames.map((game) => (
+          <div key={game.id} className={classes.gameItem}>
+            <img src={game.cover}></img>
+            <h2>{game.name}</h2>
+            <p>Platform: {game.platform}</p>
+            <p>Release Date: {game.release}</p>
+            <p>Company: {game.company}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
